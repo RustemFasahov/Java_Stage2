f25c285d3aa1a5583c0e79d336d777f0cf9dc4ff
Rustem <rustem080@yandex.ru>
2019-01-29 11:24:59 +0300

    Создан класс содержащий параметры сервера.

--- src/Test.java
+++ src/Test.java
@@ -1,30 +1,47 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Test {
+
     public static void main(String[] args) {
+        //По идее нужно делать сервер многопоточным. Создать отдельный класс реализовать в нем интерфейс Runnuble.
 
-        final int size = 100000000;
-        final int h = size/2;
-        float[] arr = new float[size];
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = 1;
+        try {
+            ServerSocket serverSocket = new ServerSocket(8182);
+            while (true){
+                Socket socket = serverSocket.accept();
+                new Thread(new Line(socket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        long a = System.currentTimeMillis();
+//        Реализация сервера без многопоточности.
 
-        float[] a1 = new float[h];
-        float[] a2 = new float[h];
-        System.arraycopy(arr,0, a1, 0, h);
-        System.arraycopy(arr, h, a2, 0, h);
-
-        new SecondThread(a2).start();
-
-        for (int i = 0; i < a1.length; i++) {
-            a1[i] = (float)(a1[i]*Math.sin(0.2f+i/5)*Math.cos(0.2f+i/5)*Math.cos(0.4f+i/2));
-        }
-
-        System.arraycopy(a1,0, arr, 0, h);
-        System.arraycopy(a2,0,arr, h, h);
-
-        System.out.println("Время работы программы: " + (System.currentTimeMillis()-a)/1000 + " секунд(ы).");
+//        try {
+//            serv = new ServerSocket(8182); //На серверном сокете указывается только порт.
+//            soc = serv.accept(); //Нужен для того чтобы ждать действия пользователя.
+//            Scanner scanner = new Scanner(soc.getInputStream()); //Объект который принимает действия пользователя.
+//            PrintWriter writer = new PrintWriter(soc.getOutputStream(),true);//Объект который отсылает пользователю
+//            //Нужно писать autoFlush - true, для того чтобы сразу очищался буфер обмена.
+//
+//            while (true){
+//                String str = scanner.nextLine(); //В данном случае считывается введенная строчка пользователя.
+//                if(str.equals("exit")) break; //Если присылают слово exit то бесконечный цикл прерывается и сервер закрывается.
+//                writer.println("Сервер " + str); //Данная строчка отправляет пользователю ответ.
+//                System.out.println(str); //Для отслеживания работы программы. Вывод в консоль.
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                serv.close(); //Закрываем сервер для того чтобы не грузила систему.
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 }
