7a3a6e43f5d6efa4c053ab56e8dd6d256b1a5a32
Rustem <rustem080@yandex.ru>
2019-02-09 22:22:43 +0300

    Добавлены поправки периода ожидания.

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -9,27 +9,34 @@ public class ClientHandler {
     private Scanner in;
     private PrintWriter out;
     private String name;
+    private boolean conection;
+    private float lastTime;
+    private float carentTime;
+    private float deltaTime;
 
     public String getName(){
         return name;
     }
 
     public ClientHandler(MyServer myServer, Socket socket){
+        conection = true;
+        lastTime = System.nanoTime();
         this.myServer = myServer;
         this.socket = socket;
         try {
             in = new Scanner(socket.getInputStream());
             out = new PrintWriter(socket.getOutputStream(), true);
             name = "";
+
             new Thread(()-> {
                 try {
                     while (true) {
                         String str = in.nextLine();
                         if (str.startsWith("/auth")) {
                             String[] parts = str.split("\\s");
-                            String nick = myServer.getAuthService().getNickByLoginPass(parts[1],parts[2]);
-                            if(nick != null){
-                                if(!myServer.isNickBusy(nick)){
+                            String nick = myServer.getAuthService().getNickByLoginPass(parts[1], parts[2]);
+                            if (nick != null) {
+                                if (!myServer.isNickBusy(nick)) {
                                     sendMessage("/authok" + nick);
                                     name = nick;
                                     myServer.subscribe(this);
@@ -38,26 +45,41 @@ public class ClientHandler {
                                 } else {
                                     sendMessage("Учетная запись используется.");
                                 }
-                            }else {
+                            } else {
                                 sendMessage("Неверный логин/пароль.");
                             }
                         }
                     }
-                    while (true){
+                    while (true) {
+                        if (conection == false) break;
                         String str = in.nextLine();
-                        if(str.equals("/end"))break;
+                        if (str.equals("/end")) break;
                         myServer.broadCastMsg(name + ":" + str);
                     }
-                } finally {
+                }finally {
                     myServer.broadCastMsg(name + "вышел из чата.");
                     myServer.unsubscribe(this);
                     try {
+
                         socket.close();
+                        in.close();
+                        out.close();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
             }).start();
+
+            while (true) {
+                carentTime = System.nanoTime();
+                deltaTime = (carentTime - lastTime) / 1000000000;
+                if (deltaTime > 120) {
+                    conection = false;
+                    sendMessage("Ошибка авторизации перезапустите программу.");
+                    socket.close();
+                    break;
+                }
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
